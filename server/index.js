63f8c67e2a2fd32e5e026661d4e49f5df77ef8d9
AgamMalik <agam.malik31@gmail.com>
2024-03-05 15:43:58 +0530

    did file storage configuration(multer)

--- server/index.js
+++ server/index.js
@@ -14,14 +14,33 @@ import { fileURLToPath } from "url";
 
 // to grab the file url
 const __filename = fileURLToPath(import.meta.url);
+
+// to use directory name
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// to log requests, errors etc to the console
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+// to store assets(images) locally
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+
+// file storage configuration
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
